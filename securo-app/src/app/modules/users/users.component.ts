973452652622774cb613bbeace9ce0c0866a82b7
vozniack <t.vozniackk@gmail.com>
2024-12-23 21:38:27 +0100

    After merge fixes

--- securo-app/src/app/modules/users/users.component.ts
+++ securo-app/src/app/modules/users/users.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { fadeInOutAnimation } from '../../shared/animations/fade-in-out-animation';
-import { InputComponent } from '../../shared/components/forms/input/input.component';
+import { TextInputComponent } from '../../shared/components/forms/text-input/text-input.component';
 
 @Component({
   selector: 'sec-users',
   standalone: true,
-  imports: [TranslatePipe, InputComponent],
+  imports: [TranslatePipe, TextInputComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
   animations: [fadeInOutAnimation]
