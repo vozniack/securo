9595b78477203024d348ad6f9cb8f6ecda280d72
vozniack <t.vozniackk@gmail.com>
2025-01-05 21:03:27 +0100

    Updated UI for mobile devices

--- securo-app/src/environments/environment.ts
+++ securo-app/src/environments/environment.ts
@@ -2,5 +2,5 @@ import {EnvironmentProperties} from './model/environment.interface';
 
 export const environment: EnvironmentProperties = {
   production: false,
-  backendUrl: 'http://localhost:8080/api',
+  backendUrl: 'http://192.168.1.15:8080/api',
 };
