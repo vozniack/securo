d584add1bf2f2c0e859c25d5a5bc7eb3987c5585
vozniack <t.vozniackk@gmail.com>
2025-01-05 22:37:21 +0100

    Changed environment url

--- securo-app/src/environments/environment.ts
+++ securo-app/src/environments/environment.ts
@@ -2,5 +2,5 @@ import {EnvironmentProperties} from './model/environment.interface';
 
 export const environment: EnvironmentProperties = {
   production: false,
-  backendUrl: 'http://192.168.1.15:8080/api',
+  backendUrl: 'http://localhost:8080/api',
 };
